1d662a74cd9478ba11cebddc44cb14ac36275ab5
iarba <dociarba@gmail.com>
2019-10-06 10:36:18 +0300

    initialising wires in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
 #include "manipulator.h"
+#include "wire.h"
 
 int main(int argc, char **argv)
 {
   printf("I believe I can fly!\n");
+  wires_init();
   Manipulator man = Manipulator();
   man.start();
   return 0;
--- src/wire.h
+++ src/wire.h
@@ -1,6 +1,8 @@
 #ifndef WIRE_H
 #define WIRE_H
 
+void wires_init();
+
 int wire_init(int id);
 
 void wire_turn_on(int id);
